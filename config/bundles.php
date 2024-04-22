e275b40884827dbe63b4a3d913361dcb2b0f6fcc
Stefan Aritonovic <stefan@paprikart.co.rs>
2024-04-22 21:30:44 +0200

    Setup webpack and tailwind css

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
